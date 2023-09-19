8b6735f1e549d2684d03db3d5bce065c25cdd8d0
Eloisa Fernandes <84110660+eloisafernands@users.noreply.github.com>
2023-09-19 12:44:49 -0300

    Add files via upload

---
+++ general.js
@@ -0,0 +1,5 @@
+// Adicione um event listener para o botão do menu mobile
+document.querySelector(".buttonMenuMobile").addEventListener("click", function () {
+    // Alterne a classe "menuMobileAberto" no body
+    document.body.classList.toggle("menuMobileAberto");
+});
